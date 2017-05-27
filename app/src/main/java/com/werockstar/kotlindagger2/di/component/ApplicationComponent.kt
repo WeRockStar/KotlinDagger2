be4c2eca4eabb7d62a1a4d686d61e4347da58ed8
WeRockStar <kotchaphan.m@gmail.com>
2017-05-28 02:46:46 +0700

    Update activity module

--- app/src/main/java/com/werockstar/kotlindagger2/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/werockstar/kotlindagger2/di/component/ApplicationComponent.kt
@@ -1,5 +1,7 @@
 package com.werockstar.kotlindagger2.di.component
 
+import com.werockstar.kotlindagger2.di.module.ActivityModule
+import com.werockstar.kotlindagger2.di.module.ApplicationModule
 import com.werockstar.kotlindagger2.di.module.HttpModule
 import dagger.Component
 import javax.inject.Singleton
@@ -8,7 +10,7 @@ import javax.inject.Singleton
  * Created by werockstar on 5/28/2017 AD.
  */
 @Singleton
-@Component(modules = arrayOf(HttpModule::class))
+@Component(modules = arrayOf(HttpModule::class, ApplicationModule::class, ActivityModule::class))
 interface ApplicationComponent {
 
 }
--- app/src/main/java/com/werockstar/kotlindagger2/di/module/ActivityModule.kt
+++ app/src/main/java/com/werockstar/kotlindagger2/di/module/ActivityModule.kt
@@ -1,9 +1,7 @@
 package com.werockstar.kotlindagger2.di.module
 
 import android.app.Activity
-import android.app.Application
 import com.werockstar.kotlindagger2.di.ActivityContext
-import com.werockstar.kotlindagger2.di.ApplicationContext
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -12,10 +10,10 @@ import javax.inject.Singleton
  * Created by werockstar on 5/28/2017 AD.
  */
 @Module
-class AndroidModule(var activity: Activity) {
+class ActivityModule(var activity: Activity) {
 
     @Provides
     @Singleton
     @ActivityContext
-    fun provideApplication(): Activity = activity
+    fun provideActivity(): Activity = activity
 }
