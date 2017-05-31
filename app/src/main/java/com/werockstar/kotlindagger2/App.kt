5173a06a29dcf70cb1d860c775f024b5acfe2368
WeRockStar <kotchaphan.m@gmail.com>
2017-06-01 01:29:41 +0700

    Add application class

---
+++ app/src/main/java/com/werockstar/kotlindagger2/App.kt
@@ -0,0 +1,14 @@
+package com.werockstar.kotlindagger2
+
+import android.app.Application
+
+/**
+ * Created by werockstar on 6/1/2017 AD.
+ */
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
