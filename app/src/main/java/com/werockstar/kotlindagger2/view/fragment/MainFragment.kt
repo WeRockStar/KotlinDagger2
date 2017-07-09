efbdebf2f78b6711710ac986a86998320d1c0902
WeRockStar <kotchaphan.m@gmail.com>
2017-07-09 16:12:35 +0700

    Move inject into onAttach

--- app/src/main/java/com/werockstar/kotlindagger2/view/fragment/MainFragment.kt
+++ app/src/main/java/com/werockstar/kotlindagger2/view/fragment/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.werockstar.kotlindagger2.view.fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -13,12 +14,15 @@ import com.werockstar.kotlindagger2.view.activity.MainActivity
 
 class MainFragment : Fragment() {
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        getMainActivity().component().inject(this)
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_main, container, false)
-
-        getMainActivity().component().inject(this)
 
         return view
     }
