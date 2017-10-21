e8d2daf7bb187a8c98b0cfaa9c760f118f1b54af
WeRockStar <kotchaphan.m@gmail.com>
2017-10-21 21:44:18 +0700

    Downgrade gradle script

--- app/src/main/java/com/werockstar/kotlindagger2/di/module/ActivityModule.kt
+++ app/src/main/java/com/werockstar/kotlindagger2/di/module/ActivityModule.kt
@@ -5,7 +5,6 @@ import com.werockstar.kotlindagger2.di.ActivityContext
 import com.werockstar.kotlindagger2.di.ActivityScope
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 
 /**
  * Created by werockstar on 5/28/2017 AD.
--- app/src/main/java/com/werockstar/kotlindagger2/view/fragment/MainFragment.kt
+++ app/src/main/java/com/werockstar/kotlindagger2/view/fragment/MainFragment.kt
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.werockstar.kotlindagger2.R
 import com.werockstar.kotlindagger2.extension.toClassName
 import com.werockstar.kotlindagger2.view.activity.MainActivity
