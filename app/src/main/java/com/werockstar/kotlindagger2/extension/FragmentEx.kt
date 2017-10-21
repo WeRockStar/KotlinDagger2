b249f214dd6708d10ccfa93a96fbec8042b050b0
WeRockStar <kotchaphan.m@gmail.com>
2017-10-21 21:32:02 +0700

    Update travis ci script

---
+++ app/src/main/java/com/werockstar/kotlindagger2/extension/FragmentEx.kt
@@ -0,0 +1,5 @@
+package com.werockstar.kotlindagger2.extension
+
+/**
+ * Created by werockstar on 10/21/2017 AD.
+ */
---
+++ app/src/test/java/com/werockstar/kotlindagger2/extension/FragmentExTest.kt
@@ -0,0 +1,8 @@
+package com.werockstar.kotlindagger2.extension
+
+import org.junit.Assert.*
+
+/**
+ * Created by werockstar on 10/21/2017 AD.
+ */
+class FragmentExTest
