07dda4ab151f9e4bf6aa2fc13c0a7beefc996245
WeRockStar <kotchaphan.m@gmail.com>
2018-01-17 22:34:59 +0700

    Update target sdk and build tools

--- app/src/main/java/com/werockstar/kotlindagger2/view/fragment/MainFragment.kt
+++ app/src/main/java/com/werockstar/kotlindagger2/view/fragment/MainFragment.kt
@@ -19,9 +19,9 @@ class MainFragment : Fragment() {
         getMainActivity().component().inject(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater!!.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
 
         return view
     }
